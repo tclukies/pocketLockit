f7abe96958f93f837ff8f61e3c236ca22f5dc925
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-21 21:36:40 -0600

    trying to activate pins on click

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
 var io= require("socket.io-client")
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out'); // gpio 4 as out
 
 var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
 //   socket.emit('chat message', $('#m').val());
@@ -13,10 +15,12 @@ socket.on("unlock now", onUnlock)
 
 function onUnlock(id){
     console.log("onUnlock", id)
+    LED.writeSync(1);
 }
 
 socket.on("lock now", onLock)
 
 function onLock(id){
     console.log("onLock", id)
+    LED.writeSync(0)
 }
