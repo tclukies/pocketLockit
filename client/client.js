95bb092f538ef1d9cc7ed0a71ee1a17207c9efb4
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-09-23 21:24:31 -0600

    adding login page

--- client/client.js
+++ client/client.js
@@ -1,3 +1,34 @@
+var loginCredentials;
+
+function accessAPI() {
+    fetch("https://pocket-lockit.herokuapp.com/profiles")
+        .then(response => response.json())
+        // .then(response => {data = response})
+        .then(getData);
+}
+accessAPI();
+
+function getData(data) {
+    loginCredentials = data;
+}
+
+document.querySelector(".submit").addEventListener("click", compareCredentials);
+
+function compareCredentials(event) {
+    event.preventDefault();
+    if (
+        event.path[1][0].value === loginCredentials.profile[0].username &&
+        event.path[1][1].value === loginCredentials.profile[0].password
+    ) {
+        document.querySelector(".signin").classList.add("hidden")
+        document.querySelector(".lock-main").classList.remove("hidden")
+    } else {
+        console.log("false");
+        document.querySelector(".invalid-login").textContent =
+            "Incorrect Login. Please Try Again";
+    }
+}
+
 
 var socket = io();
 
