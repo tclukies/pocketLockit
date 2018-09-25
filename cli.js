af5c28f1cc0d72bb571df10900a194dc89c85003
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-25 15:07:11 -0600

    moving function outside of lock function

--- cli.js
+++ cli.js
@@ -4,7 +4,8 @@ const lock = new Gpio(4, 'out'); // gpio 4 as out
 const motion = new Gpio(13, 'in'); // gpio 4 as in
 const LED = new Gpio(25, 'out'); // gpio 4 as out
 
-
+let currentValue
+let lastValue
 
 var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
 //   socket.emit('chat message', $('#m').val());
@@ -22,24 +23,24 @@ function onUnlock(id){
     let lastValue = motion.readSync();
     console.log("lastValue" + lastValue)
     
-    iv = setInterval(function() {
-        currentValue = motion.readSync();
-        if (currentValue != lastValue){
-            socket.emit('motion now', 1);
-            console.log("tampered!!!!!!")
-            console.log(currentValue + " " +lastValue)
-            lastValue = currentValue
-            const timer = setInterval(()=>{
-                if (LED.readSync() === 0) { // if current pin state is 0 (off)
-                    LED.writeSync(1); // make it 1 (on)
-                } else {
-                    LED.writeSync(0); // make it 0 (off)
-                }
-            }, 1000);
-        
-        }
-        }, 200);
 }
+        iv = setInterval(function() {
+            currentValue = motion.readSync();
+            if (currentValue != lastValue){
+                socket.emit('motion now', 1);
+                console.log("tampered!!!!!!")
+                console.log(currentValue + " " +lastValue)
+                lastValue = currentValue
+                const timer = setInterval(()=>{
+                    if (LED.readSync() === 0) { // if current pin state is 0 (off)
+                        LED.writeSync(1); // make it 1 (on)
+                    } else {
+                        LED.writeSync(0); // make it 0 (off)
+                    }
+                }, 1000);
+            
+            }
+            }, 200);
 
 socket.on("lock now", onLock)
 
