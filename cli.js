f47a57c97955a9bf22dcdb6a93ad0b638f25316d
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-25 12:14:40 -0600

    working adding led functionality

--- cli.js
+++ cli.js
@@ -2,6 +2,8 @@ var io= require("socket.io-client")
 const Gpio = require('onoff').Gpio;
 const lock = new Gpio(4, 'out'); // gpio 4 as out
 const motion = new Gpio(13, 'in'); // gpio 4 as in
+const LED = new Gpio(25, 'out'); // gpio 4 as out
+
 
 
 var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
@@ -39,13 +41,13 @@ iv = setInterval(function() {
         console.log("tampered!!!!!!")
         console.log(currentValue + " " +lastValue)
         lastValue = currentValue
-        // const timer = setInterval(()=>{
-        //     if (LED.readSync() === 0) { // if current pin state is 0 (off)
-        //       LED.writeSync(1); // make it 1 (on)
-        //     } else {
-        //       LED.writeSync(0); // make it 0 (off)
-        //     }
-        //   }, 1000);
+        const timer = setInterval(()=>{
+            if (LED.readSync() === 0) { // if current pin state is 0 (off)
+              LED.writeSync(1); // make it 1 (on)
+            } else {
+              LED.writeSync(0); // make it 0 (off)
+            }
+          }, 1000);
     
     }
     }, 200);
