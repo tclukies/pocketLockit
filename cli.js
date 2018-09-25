c69dbc5db0797088b5bb8e77fcb0ae03b1f92b58
Thomas Clukies <thomas.clukies@gmail.com>
2018-09-25 15:26:32 -0600

    trying a new check to see if lock is locked to set motion sensor position

--- cli.js
+++ cli.js
@@ -4,11 +4,11 @@ const lock = new Gpio(4, 'out'); // gpio 4 as out
 const motion = new Gpio(13, 'in'); // gpio 4 as in
 const LED = new Gpio(25, 'out'); // gpio 4 as out
 
-let currentValue
-let lastValue
+let lockStatus
 
 var socket = io(process.env.SOCKET_HOST || "http://localhost:3000");
 //   socket.emit('chat message', $('#m').val());
+
 
 // lock()
 
@@ -17,34 +17,37 @@ socket.on("unlock now", onUnlock)
 function onUnlock(id){
     console.log("onUnlock", id)
     lock.writeSync(0);
-    currentValue = motion.readSync();
-    console.log("currentValue" + currentValue)
-    
+    lockStatus = unlocked
 }
-lastValue = motion.readSync();
-console.log("lastValue" + lastValue)
-
-        iv = setInterval(function() {
-            currentValue = motion.readSync();
-            if (currentValue != lastValue){
-                socket.emit('motion now', 1);
-                console.log("tampered!!!!!!")
-                console.log(currentValue + " " +lastValue)
-                lastValue = currentValue
-                const timer = setInterval(()=>{
-                    if (LED.readSync() === 0) { // if current pin state is 0 (off)
-                        LED.writeSync(1); // make it 1 (on)
-                    } else {
-                        LED.writeSync(0); // make it 0 (off)
-                    }
-                }, 1000);
-            
-            }
-            }, 200);
 
 socket.on("lock now", onLock)
 
 function onLock(id){
     console.log("onLock", id)
     lock.writeSync(1)
+    lockStatus = locked
 }
+
+let currentValue = motion.readSync();
+console.log("currentValue" + currentValue)
+
+let lastValue = motion.readSync();
+console.log("lastValue" + lastValue)
+
+iv = setInterval(function() {
+    currentValue = motion.readSync();
+    if (currentValue != lastValue && lockStatus === locked){
+        socket.emit('motion now', 1);
+        console.log("tampered!!!!!!")
+        console.log(currentValue + " " +lastValue)
+        lastValue = currentValue
+        const timer = setInterval(()=>{
+            if (LED.readSync() === 0) { // if current pin state is 0 (off)
+                LED.writeSync(1); // make it 1 (on)
+            } else {
+                LED.writeSync(0); // make it 0 (off)
+            }
+        }, 1000);
+    
+    }
+    }, 200);
